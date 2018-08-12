4612fb0f41a4a2c34fa0c9421e036ab26fd7d46f
Khoa1392 <s3330365@student.rmit.edu.au>
2018-08-12 23:16:56 +1000

    Change the intervals of the job for testing purposes
    To check it is working as intended before changing it to longer intervals.

--- 3i_cron.py
+++ 3i_cron.py
@@ -7,6 +7,6 @@ cron.remove_all()
 #add new cron job
 job  = cron.new(command='/home/pi/A01/3idemo.py')
 
-#job settings to run every 6 hours
-job.hour.every(6)
+#job settings to run every minute (currently for testing purposes)
+job.minute.every(1)
 cron.write()

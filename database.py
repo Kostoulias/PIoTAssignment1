9344f82ddcfeedf0858d56d3b46738888b4b2192
James Kostoulias <s3627782@student.rmit.edu.au>
2018-08-12 22:16:45 +1000

    database.py
    apparently this didnt update with it all

--- database.py
+++ database.py
@@ -3,5 +3,5 @@ import sys
 con = lite.connect('database.db')
 with con: 
     cur = con.cursor() 
-    cur.execute("DROP TABLE IF EXISTS database_data")
-    cur.execute("CREATE TABLE database_data(timestamp DATETIME, temp NUMERIC, humidity NUMERIC)")
+    cur.execute("DROP TABLE IF EXISTS DATABASE_data")
+    cur.execute("CREATE TABLE DATABASE_data(timestamp DATETIME, temp NUMERIC, humidity NUMERIC)")

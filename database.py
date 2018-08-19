2487531b5c0d674ce01e1466f69301e8518ec23a
Khoa1392 <s3330365@student.rmit.edu.au>
2018-08-19 23:17:29 +1000

    Comment on db file

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import sqlite3 as lite
 import sys
+#Connect to database file
 con = lite.connect('database.db')
+#Drops table if it exists and create a database with the following attributes
 with con: 
     cur = con.cursor() 
     cur.execute("DROP TABLE IF EXISTS DATABASE_data")
